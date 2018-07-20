4b3f000a8c88710c60a28c7cdf00c829c98069fb
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-20 17:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,44 +56,11 @@ function removeFromCart(item) {
   }
 }
 
-
-
+function placeOrder(cardNumber) {
+  
+}
 
 /*
-describe("removeFromCart()", function() {
-  it("removes the specified item from the cart", function() {
-    addToCart("vanilla");
-    addToCart("watermelon");
-    addToCart("yams");
 
-    removeFromCart("watermelon");
-
-    const firstItemName = getCart()[0].itemName;
-    const secondItemName = getCart()[1].itemName;
-
-    expect(firstItemName).toEqual("vanilla");
-    expect(secondItemName).toEqual("yams");
-
-    removeFromCart("yams");
-
-    expect(getCart().length).toEqual(1);
-  });
-
-  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
-    // Repeat item name from previous test to prevent hard-coding.
-    expect(removeFromCart("yams")).toEqual("That item is not in your cart.");
-  });
-});
-
-
-
-4. The `removeFromCart()` function accepts one argument, the name of the item that should be removed.
-    + If the item *is* present in the cart, the function should remove the object from the cart and then return the updated cart.
-    + ***HINT***: Check each object's itemName value key to see if it matches the parameter, then remove it if it matches. You might find [Array.prototype.splice()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to be useful.
-    + If the cart does not contain a matching item, the function should return `That item is not in your cart.`
 */
 
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
