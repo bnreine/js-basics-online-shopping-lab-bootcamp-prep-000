ff64a75b5f3d4d6a3bcf7b5c602d4dd9b9442145
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-20 17:02:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ function viewCart() {
 
 /*
 
+
+
+
+2. The `viewCart()` function does not accept any arguments. It should loop over every item in your cart, returning the contents as one long, coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+    + If the cart is empty, the function should instead return `Your shopping cart is empty.`
+    + ***Note***: Pay close attention to the syntax above. The returned statement should be a single sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes according to how many items the cart contains:
+      * 1 item — `In your cart, you have bananas at $17.`
+      * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
+      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
 */
 
 
