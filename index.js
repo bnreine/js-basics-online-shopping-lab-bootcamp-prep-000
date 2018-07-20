3c163fd0ae3f46c5fdaa14c6df74925c260dc9d6
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-20 18:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber===)
+  if (arguments.length===0) {
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
+  else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart=[];
+  }
 }
 
 /*
