7754f7044e43b796e0608d2cc8a191632a6b2fc2
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-20 17:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,57 +34,7 @@ function viewCart() {
 
 
 /*
-    + If the cart is empty, the function should instead return `Your shopping cart is empty.`
-    + ***Note***: Pay close attention to the syntax above. The returned statement should be a single sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes according to how many items the cart contains:
-      * 1 item — `In your cart, you have bananas at $17.`
-      * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
-      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
 
-
-
-
-
-
-describe("viewCart()", function() {
-  it("prints 'Your shopping cart is empty.' if the cart is empty", function() {
-    expect(viewCart()).toEqual("Your shopping cart is empty.")
-  });
-
-  it("correctly prints a one-item cart", function() {
-    addToCart("lemons");
-    expect(viewCart()).toEqual(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
-  });
-
-  it("correctly prints a two-item cart", function() {
-    addToCart("mango");
-    addToCart("nuts");
-
-    expect(viewCart()).toEqual(
-      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
-    );
-  });
-
-  it("correctly prints a three-or-more-item cart", function() {
-    addToCart("orange");
-    addToCart("pear");
-    addToCart("quince");
-
-    ;
-
-    expect(viewCart()).toEqual(
-      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
-    );
-  });
-});
-
-
-
-2. The `viewCart()` function does not accept any arguments. It should loop over every item in your cart, returning the contents as one long, coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
-    + If the cart is empty, the function should instead return `Your shopping cart is empty.`
-    + ***Note***: Pay close attention to the syntax above. The returned statement should be a single sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes according to how many items the cart contains:
-      * 1 item — `In your cart, you have bananas at $17.`
-      * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
-      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
 */
 
 
