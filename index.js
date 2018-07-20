2b92ea0efb2e6cd832464ab578524127ecd9ed2b
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-20 17:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,42 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  
 }
 
-/*
 
+/*
+describe("viewCart()", function() {
+  it("prints 'Your shopping cart is empty.' if the cart is empty", function() {
+    expect(viewCart()).toEqual("Your shopping cart is empty.")
+  });
+
+  it("correctly prints a one-item cart", function() {
+    addToCart("lemons");
+    expect(viewCart()).toEqual(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
+  });
+
+  it("correctly prints a two-item cart", function() {
+    addToCart("mango");
+    addToCart("nuts");
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    );
+  });
+
+  it("correctly prints a three-or-more-item cart", function() {
+    addToCart("orange");
+    addToCart("pear");
+    addToCart("quince");
+
+    ;
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
+    );
+  });
+});
 
 
 
